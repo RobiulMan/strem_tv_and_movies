32c71919f67443183eec49f6994f8df9241793aa
unknown <robiulislam.bg@gmail.com>
2021-07-08 03:31:46 +0600

    todys I've completed nav

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import Header from "../components/Header";
-import styles from "../styles/Home.module.css";
+import Nav from "../components/Nav";
 
 export default function Home() {
   return (
@@ -11,6 +11,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
+      <Nav />
     </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,14 @@
 // tailwind.config.js
 module.exports = {
-mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  mode: "jit",
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {}
   },
   variants: {
-    extend: {},
+    extend: {}
     // animation: ['responsive', 'motion-safe', 'motion-reduce','animate-bounce']
   },
-  plugins: [],
-}
+  plugins: [require("tailwind-scrollbar-hide")]
+};
